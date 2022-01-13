07773368c13f9bcf234e20dc179d1d7210a7f46c
G0rian <moritzweixler@outlook.de>
2022-01-13 18:51:38 +0100

    test: fix angle check for edges

--- test/hive_test.cpp
+++ test/hive_test.cpp
@@ -20,18 +20,26 @@ int main(void) {
     std::cout << "====== " << (int) index << " ====== " << std::endl;
     for (uint8_t listIndex = 0; listIndex < nextEdgeIndices.size(); listIndex++) {
       uint8_t nextEdgeIndex = nextEdgeIndices.at(listIndex);
-      std::cout << "    " << (int) nextEdgeIndex;
+      std::cout << (int) nextEdgeIndex << " : ";
       const Edge nextEdge = Hive::getInstance().getEdgeByIndex(nextEdgeIndex);
       //std::cout << (int) edge.getStartLedIndex() << " -> " << (int) nextEdge.getStartLedIndex() << std::endl;
-      //std::cout << edge.getAngle() * 60 << "\370 -> " << nextEdge.getAngle() * 60 << "\370" << std::endl;
-      if ((edge.getAngle() - nextEdge.getAngle()) % 1 != 0) {
-        //std::cout << "ERROR" << std::endl;
-        return EXIT_FAILURE;
+      std::cout << edge.getAngle() * 60 << "\370 -> " << nextEdge.getAngle() * 60 << "\370" << std::endl;
+      int16_t deltaAngle = (int16_t) edge.getAngle() * 60 - (int16_t) nextEdge.getAngle() * 60;
+      if (deltaAngle < 0) {
+        deltaAngle = deltaAngle + 360;
+      }
+      if (deltaAngle % 120 == 0) {
+        std::cout << "different directions " << edge.getNextDirs().at(listIndex) << std::endl;
+        if (edge.getNextDirs().at(listIndex) != 0) {
+          std::cout << "Test failed!" << std::endl;
+          return EXIT_FAILURE;
+        }
       } else {
-        //std::cout << "different directions" << std::endl;
+        std::cout << "same directions " << edge.getNextDirs().at(listIndex) << std::endl;
       }
       std::cout << std::endl;
     }
   }
+  std::cout << "Test succeeded!" << std::endl;
   return EXIT_SUCCESS;
 }
